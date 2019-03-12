b98f0cde786475a76c144567cc41b7edd525e385
pirek5 <pir.czajkowski@gmail.com>
2019-03-12 14:00:17 +0100

    working simple map generator and map display

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapDisplay : MonoBehaviour
+{
+    //config
+    [SerializeField] private TileBase fullTile;
+    [SerializeField] private TileBase backgroundTile;
+
+    //references set in editor
+    [SerializeField] private Tilemap foreground;
+    [SerializeField] private Tilemap background;
+
+
+    public void DisplayMap(Dictionary<Vector3Int, TileInfo> gridDictionary)
+    {
+        foreach(KeyValuePair<Vector3Int, TileInfo> tile in gridDictionary)
+        {
+            background.SetTile(tile.Key, backgroundTile);
+            foreground.SetTile(tile.Key, fullTile);
+        }
+    }
+}
